fe02604104f383b0388db5af38cd2a9e322e09b3
Ruhul Amin <ruhulaminswapon93@gmail.com>
2023-08-15 22:16:26 +0600

    ass

--- learn.js
+++ learn.js
@@ -1,4 +1,11 @@
-console.log("Hello World");
-console.log("Learning JavaScrip From Zero to Advanced");
-console.log("Learning JavaScrip From Zero to Advanced");
-console.log("Learning JavaScrip From Zero to Advanced");
+//console.log("Hello World");
+//console.log("Learning JavaScrip From Zero to Advanced");
+//console.log("Learning JavaScrip From Zero to Advanced");
+//console.log("Learning JavaScrip From Zero to Advanced");
+
+
+var num1 = 30.5;
+var num2 = 11.8;
+var sum = num1 + num2
+
+console.log(sum);
