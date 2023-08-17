4a86876fa803b32f22bfc94041a72915a232adb3
Ruhul Amin <ruhulaminswapon93@gmail.com>
2023-08-17 20:43:08 +0600

    add

--- learn.js
+++ learn.js
@@ -17,3 +17,16 @@ console.log("tmi young aso miya");
 else{
     console.log("tmi buira na");
 }
+var age = 30;
+
+if (age > 50){
+    console.log("tmi akta buira");
+}
+
+else if(age < 40){
+console.log("tmi young aso miya");
+}
+
+else{
+    console.log("tmi buira na");
+}
