6d21b1d0486cd1f43bf4e1f2986dc33e437e50ce
Ruhul Amin <ruhulaminswapon93@gmail.com>
2023-08-19 00:19:25 +0600

    add

--- learn.js
+++ learn.js
@@ -4,37 +4,47 @@
 //console.log("Learning JavaScrip From Zero to Advanced");
 
 
-var age = 30;
+//var age = 30;
 
-if (age > 50){
-    console.log("tmi akta buira");
-}
+//if (age > 50){
+ //   console.log("tmi akta buira");
+//}
 
-else if(age < 40){
-console.log("tmi young aso miya");
-}
+//else if(age < 40){
+//console.log("tmi young aso miya");
+//}
 
-else{
-    console.log("tmi buira na");
-}
-var age = 30;
+//else{
+ //   console.log("tmi buira na");
+//}
+//var age = 30;
 
-if (age > 50){
-    console.log("tmi akta buira");
-}
+//if (age > 50){
+ //   console.log("tmi akta buira");
+//}
 
-else if(age < 40){
-console.log("tmi young aso miya");
-}
+//else if(age < 40){
+//console.log("tmi young aso miya");
+//}
 
-else{
-    console.log("tmi buira na");
-}
+//else{
+ //   console.log("tmi buira na");
+//}
 
-var person = {
-    name: "Amin",
-    age: 27,
-    status: "Marred",
-}
+//var person = {
+  //  name: "Amin",
+   // age: 27,
+  //  status: "Marred",
+//}
 
-console.log(typeof person);
+//console.log(person);
+
+//var arrayAce = [1, 2, 31, 52];
+
+//arrayAce.shift();
+
+//console.log(arrayAce);
+
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers);
